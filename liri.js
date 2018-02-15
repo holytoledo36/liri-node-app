79c54fe62f096654c32e56bc6f9ed916d929c1ce
David Morrill <judimorrill@airbears2-10-142-116-160.airbears2.1918.berkeley.edu>
2018-02-14 20:06:23 -0800

    fixed bugs

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-
 var keys = require ("./keys.js");
 
 var Twitter = require ("twitter");//mpm package for twitter
@@ -13,6 +12,8 @@ var Spotify = require("node-spotify-api");
 
 var request = require ('request');
 
+
+
 //var dataArr = [];
 var spotify = new Spotify ({
 	id: "863455d15a1f48568b17e5c824abc58a",
@@ -20,19 +21,10 @@ var spotify = new Spotify ({
 })
 
 var twitterFunc = function () {
-//	var twitter = new Twitter ({
-// 		consumer_key: keys.twitterKeys.consumer_key,
-// 	  	consumer_secret: keys.twitterKeys.consumer_secret,
-// 	  	access_token_key: keys.twitterKeys.access_token_key,
-// 	  	access_token_secret: keys.twitterKeys.access_token_secret
-// });
+
 	var client = new Twitter (keys.twitterKeys);
-
-
-
-
 	var params = {
-		screen_name: 'jesus',
+		screen_name: 'D_Copperfield',
 		count: 20
 	};
 
@@ -41,21 +33,24 @@ var twitterFunc = function () {
 	  		for (var i=0; i<tweets.length; i++) {
 	    		console.log(tweets[i].text);
 	    		console.log('-----------------');
-//	    		dataArr.push(tweets[i].text);
+	    		console.log('');
+	    		console.log(tweets[i].text);
+	    	}	
 
-	    		}	
+	    }	
 	  	
-	  	}
+})
 
-	  		//appendFunc();
-	  	});
+		fs.appendFile("log.txt", "\n" + params.screen_name, function(err) {
+			if (err) {
+				return console.log(err);
+		}
+	});
 }
 
 
-
-
-
-var spotifyFunc = function() {
+function spotifyPlay(spotifySong) {
+	
 	spotifySong = userInput;
 	// if user doesn't input a song, default to The Sign 
 	if (spotifySong === undefined) {
@@ -110,6 +105,32 @@ switch (userRequest){
 	default:
 		console.log("chose correct option");
 }
+
+
+
+
+
+
+
+//Link to keys.js
+//process.rv
+//look into using switch
+//twitter require
+//require spotify
+//require fs 
+
+
+//make code so shows last 20 tweets and when created
+//This will show the following information about the song in your terminal/bash window
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+// If no song is provided then your program will default to "The Sign" by Ace of Base
+
+//Look up request
+
+//random text for do as it says, it's a read file
 
 
 
