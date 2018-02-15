f7193fc6a03cea0b8b292ae6048a8fa64211c4a2
David Morrill <judimorrill@judis-mbp.lan>
2018-02-14 23:25:33 -0800

    fixed glitches

--- liri.js
+++ liri.js
@@ -1,6 +1,23 @@
+/ //Link to keys.js
+// //process.rv
+// //look into using switch
+// //twitter require
+// //require spotify
+// //require fs 
+
+
+// //make code so shows last 20 tweets and when created
+// //This will show the following information about the song in your terminal/bash window
+// // Artist(s)
+// // The song's name
+// // A preview link of the song from Spotify
+// // The album that the song is from
+// // If no song is provided then your program will default to "I have been found" 
+
+
 var keys = require ("./keys.js");
 
-var Twitter = require ("twitter");//mpm package for twitter
+var Twitter = require ("twitter");
 
 var fs = require ("fs");
 
@@ -13,148 +30,185 @@ var Spotify = require("node-spotify-api");
 var request = require ('request');
 
 
-
-//var dataArr = [];
-var spotify = new Spotify ({
-	id: "863455d15a1f48568b17e5c824abc58a",
-	secret: "e65504051b69438c87aa284518947768"
-})
-
-var twitterFunc = function () {
-
-	var client = new Twitter (keys.twitterKeys);
+function twittyTwenty(tweet) {
+	var client = new Twitter(keys.twitterKeys);
 	var params = {
 		screen_name: 'D_Copperfield',
 		count: 20
 	};
 
-		client.get('statuses/user_timeline', params, function(error, tweets, response) {
-	  	if (!error && response.statusCode === 200) {
-	  		for (var i=0; i<tweets.length; i++) {
-	    		console.log(tweets[i].text);
-	    		console.log('-----------------');
-	    		console.log('');
-	    		console.log(tweets[i].text);
-	    	}	
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error && response.statusCode === 200) {
+			for (var i = 0; i < tweets.length; i++) {
+				console.log('----------------');
+				console.log('');
+				console.log(tweets[i].text);
+			} 
+		} 
 
-	    }	
-	  	
-})
-
-		fs.appendFile("log.txt", "\n" + params.screen_name, function(err) {
-			if (err) {
-				return console.log(err);
-		}
-	});
+	})
+	fs.appendFile("log.txt", "\n" + params.screen_name, function(err) {
+		if (err) {
+  			return console.log(err);
+    	}
+  	});
 }
 
+function spotifyPlay(songName) {
 
-function spotifyPlay(spotifySong) {
-	
-	spotifySong = userInput;
-	// if user doesn't input a song, default to The Sign 
-	if (spotifySong === undefined) {
-		spotifySong = 'The Sign';
+	var spotify = new Spotify({
+		id: "863455d15a1f48568b17e5c824abc58a",
+		secret: "e65504051b69438c87aa284518947768"
+	}); 
+
+
+	if (songName === undefined) {
+ 		songName = "I have been found";
+ 		
+ 	} 
+ 	
+ 	console.log(songName);
+ 	
+	spotify.search({ type: 'track', query: songName, limit: 3 }, 
+		function(err, data) {
+				
+	    if ( err ) {
+	        console.log('Error occurred: ' + err);
+	        return;  
+	    } else {
+		    var songInfo = data.tracks.items;
+
+		    fs.appendFile("log.txt", "\n" + songName, function(err) {
+	    		if (err) {
+	      			return console.log(err);
+		    	}
+		  	});
+		    
+		    for (var i = 0; i < songInfo.length; i++) {
+		    	for (var j = 0; j < songInfo[i].artists.length; j++) {
+		    		console.log("");
+		    		console.log("artist: ", JSON.stringify(songInfo[i].artists[j].name, null, 2));
+			    	console.log("")
+			    	console.log("------------------------------")
+		    	}
+		    	
+		    	
+		    	console.log("song: ", JSON.stringify(songInfo[i].name, null, 2));
+		    	console.log("")
+		    	console.log("------------------------------")
+		    	console.log("album: ", JSON.stringify(songInfo[i].album.name, null, 2));
+		    	console.log("")
+		    	console.log("------------------------------")
+		    	console.log("preview: ", JSON.stringify(songInfo[i].preview_url, null, 2));
+		    	console.log("")
+		    }
+	    } 
+	    
+  	});
+
+};
+
+function movieInfo(movieName) {
+
+
+	if (movieName === undefined) {
+		movieName = "Star Wars";
+	} else {
+		movieName = process.argv.slice(3).join(" ");
 	}
-	
-		spotify.search ({ 
-		type: 'track', 
-		query: spotifySong
-	}, function(error, data) {
-		if (error) {
-	    	return console.log('Error occurred: ' + error);
+
+	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+	request(queryUrl, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			console.log('');
+			console.log(JSON.parse(body).Title);
+			console.log('');
+			console.log('----------------');
+			console.log(JSON.parse(body).Year);
+			console.log('');
+			console.log('----------------');
+			console.log(JSON.parse(body).imdbRating);
+			console.log('');
+			console.log('----------------');
+			console.log(JSON.parse(body).Ratings[1].Value);
+			console.log('');
+			console.log('----------------');
+			console.log(JSON.parse(body).Country);
+			console.log('');
+			console.log('----------------');
+			console.log(JSON.parse(body).Language);
+			console.log('');
+			console.log('----------------');
+			console.log(JSON.parse(body).Plot);
+			console.log('');
+			console.log('----------------');
+			console.log(JSON.parse(body).Actors);
+			console.log('');
+			console.log('----------------');
+		} else if (error) {
+			console.log(error);
 		}
-		else {
-			for (var i=0; i<data.tracks.items.length; i++) {
-				// artist
-				console.log(data.tracks.items[i].artists[0].name);
-				// name
-				console.log(data.tracks.items[i].name);
-				// preview link to song, there are many times that it returns null so I used an if func for that
-				if (data.tracks.items[i].preview_url === null) {
-					console.log("There is no preview url for this song unfortunately");
-				}
-				else {	
-					console.log(data.tracks.items[i].preview_url);
-				}	
-				// album song is from 
-				console.log(data.tracks.items[i].album.name);
-				// divider
-				console.log('---------------------');
-				dataArr.push(
-					data.tracks.items[i].artists[0].name, 
-					data.tracks.items[i].name,
-					data.tracks.items[i].preview_url,
-					data.tracks.items[i].album.name
-				);
-			}
-			
-		}	
+
+		fs.appendFile("log.txt", "\n" + movieName, function(err) {
+    		if (err) {
+      			return console.log(err);
+	    	}
+	  	});
 	});
 }
 
-switch (userRequest){
+function doinIt() {
+	fs.readFile("random.txt", "utf8", function(error, data) {
+
+		if (error) {
+			return console.log(error);
+		}
+
+		var dataArr = data.split(",");
+
+		
+		console.log("command: ", dataArr[0]);
+		
+		var command = dataArr[0];
+		var value = dataArr[1];
+		
+		switch (command) {
+			case "spotify-this-song":
+				spotifyPlay(value);
+				break;
+			case "movie-this":
+				movieInfo(value);
+				break;
+			case "my-tweets":
+				twittyTwenty(value);
+				break;
+		};
+
+	});
+};
+ 
+switch (userRequest) {
+
 	case "my-tweets":
-		twitterFunc (userInput);
+		twittyTwenty(userInput);
 		break;
 
 	case "spotify-this-song":
-		spotifyFunc (userInput);
+		spotifyPlay(userInput);
+		break;
+
+	case "movie-this":
+		movieInfo(userInput);
+		break;
+
+	case "do-what-it-says":
+		doinIt(userInput);
 		break;
 
 	default:
-		console.log("chose correct option");
+		console.log('');
+		console.log("Please choose from your options: 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'");
+		console.log('');
 }
-
-
-
-
-
-
-
-//Link to keys.js
-//process.rv
-//look into using switch
-//twitter require
-//require spotify
-//require fs 
-
-
-//make code so shows last 20 tweets and when created
-//This will show the following information about the song in your terminal/bash window
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// If no song is provided then your program will default to "The Sign" by Ace of Base
-
-//Look up request
-
-//random text for do as it says, it's a read file
-
-
-
-
-
-
-
-//Link to keys.js
-//process.rv
-//look into using switch
-//twitter require
-//require spotify
-//require fs 
-
-
-//make code so shows last 20 tweets and when created
-//This will show the following information about the song in your terminal/bash window
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// If no song is provided then your program will default to "The Sign" by Ace of Base
-
-//Look up request
-
-//random text for do as it says, it's a read file
 
